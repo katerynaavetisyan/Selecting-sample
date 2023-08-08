c2ea6a79f1c2d9587c467a6f6912d25d38271c6b
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-08 15:40:38 -0500

    getElementsByClassName

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-const allImages = document.getElementsByTagName('img');
-
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
-}
-
-
 const squareImages = document.getElementsByClassName('square');
 
 for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+    img.src = 'https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80'
 }
 
-const links = document.querySelectorAll('p a');
-
-for (let link of links) {
-    console.log(link.href)
+for (let i = 0; i < 10; i++) {
+    console.log(i);
 }
-
-// for (let img of allImages) {
-//     console.log(img.src)
-// }
