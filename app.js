3e6de311811e4ba9cb4befb58cdc366b7b7028c5
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-08 15:42:00 -0500

    querySelectorAll

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
-const squareImages = document.getElementsByClassName('square');
 
-for (let img of squareImages) {
-    img.src = 'https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80'
-}
-
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
+console.log(document.querySelectorAll('p'));
