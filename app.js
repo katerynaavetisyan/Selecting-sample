a632d097891fd0c16361055f5149cd059029e58b
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-13 13:04:04 -0500

    innerText

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-function calculateSupply(age, numPerDay) {
-    var maxAge = 100;
-    var totalNeeded = (numPerDay * 365) * (maxAge - age);
-    var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
-    console.log(message);
-}
-
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('p').innerText);
