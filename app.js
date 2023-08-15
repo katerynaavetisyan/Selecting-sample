1cba253f4e32b314eddce8a0dabff71a55d7ab27
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-15 07:33:31 -0500

    function practice

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-
-console.log(document.querySelector('p').innerText);
+function thinking(family, me) {
+    let future = 'My lastname is ' + family + ' and my firstname is ' + me
+    console.log(future);
+}
+thinking('Avetisyan', 'Katya')
