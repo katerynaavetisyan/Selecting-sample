a95306f42b9d9039df3bc55efec7f17a6f257097
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-17 13:50:45 -0500

    a string that has letters in alphabetical order

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-function substrings(str1) {
-    let array1 = [];
-    for (let x = 0, y = 1; x < str1.length; x++, y++) {
-        array1[x] = str1.substring(x, y);
-    }
-    let combi = [];
-    let temp = "";
-    let slent = Math.pow(2, array1.length);
-
-    for (let i = 0; i < slent; i++) {
-        temp = "";
-        for (let j = 0; j < array1.length; j++) {
-            if ((i & Math.pow(2, j))) {
-                temp += array1[j];
-            }
-        }
-        if (temp !== "") {
-            combi.push(temp);
-        }
-    }
-    console.log(combi.join("\n"));
+function alphabet_order(str) {
+    return str.split('').sort().join('');
 }
-
-substrings("dog");
+console.log(alphabet_order("webmaster"));
