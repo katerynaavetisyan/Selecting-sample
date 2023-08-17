1214ff27fd3d65616bf77c5329a88ed2957c947d
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-17 13:56:17 -0500

    function that accepts a string

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-function alphabet_order(str) {
-    return str.split('').sort().join('');
+// function that accepts a string as a parameter and 
+// converts the first letter of each word into upper case.
+
+function uppercase(str) {
+    let array1 = str.split(' ');
+    let newarray1 = [];
+
+    for (let x = 0; x < array1.length; x++) {
+        newarray1.push(array1[x].charAt(0).toUpperCase()
+            + array1[x].slice(1));
+    }
+    return newarray1.join(' ');
 }
-console.log(alphabet_order("love"));
+console.log(uppercase("the quick brown fox"));
