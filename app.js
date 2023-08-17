fbfafc5db35a46307c1252b0d398b22381ce7303
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-17 14:10:11 -0500

    Magical Forest Circle Exercise

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-// function that accepts a string as a parameter and finds the longest word within the string.
+const image = document.querySelector('img');
+image.style.width = "150px";
+image.style.borderRadius = "50%";
 
-function find_longest_word(str) {
-    let array1 = str.match(/\w[a-z]{0,}/gi);
-    let result = array1[0];
-
-    for (let x = 1; x < array1.length; x++) {
-        if (result.length < array1[x].length) {
-            result = array1[x];
-        }
-    }
-    return result;
-}
-console.log(find_longest_word('Adventures are healthy and fun!'));
+const container = document.querySelector("#container");
+container.style.textAlign = "center";
