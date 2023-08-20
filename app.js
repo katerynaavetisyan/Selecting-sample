45bd00f89ff68768df8103c73eb1412736c9e845
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-20 13:54:31 -0500

    querySelector

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ image.style.width = "150px";
 image.style.borderRadius = "50%";
 
 const container = document.querySelector("#container");
-container.style.textAlign = "center";
+container.style.textAlign = "center"
