f3d3806384294245a63e7c194a5b8ecdf244fde2
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-23 10:55:44 -0500

    function Longest_Country_Name

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-function factors(n) {
-    var num_factors = [], i;
-
-    for (i = 1; i <= Math.floor(Math.sqrt(n)); i += 1)
-        if (n % i === 0) {
-            num_factors.push(i);
-            if (n / i !== i)
-                num_factors.push(n / i);
-        }
-    num_factors.sort(function (x, y) {
-        return x - y;
-    });  // numeric sort
-    return num_factors;
+function Longest_Country_Name(country_name) {
+    return country_name.reduce(function (lname, country) {
+        return lname.length > country.length ? lname : country;
+    },
+        "");
 }
-console.log(factors(15));  // [1,3,5,15] 
-console.log(factors(16));  // [1,2,4,8,16] 
-console.log(factors(17));  // [1,17]
+console.log(Longest_Country_Name(["Australia", "Germany", "United States of America"]));
