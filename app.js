75e75e2808ef483841c0d62d2d6ba93d45f449ab
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-23 10:57:03 -0500

    function longest_substring_

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-function Longest_Country_Name(country_name) {
-    return country_name.reduce(function (lname, country) {
-        return lname.length > country.length ? lname : country;
-    },
-        "");
+function longest_substring_without_repeating_characters(input) {
+    var chars = input.split('');
+    var curr_char;
+    var str = "";
+    var longest_string = "";
+    var hash = {};
+    for (var i = 0; i < chars.length; i++) {
+        curr_char = chars[i];
+        if (!hash[chars[i]]) {
+            str += curr_char;
+            hash[chars[i]] = { index: i };
+        }
+        else {
+            if (longest_string.length <= str.length) {
+                longest_string = str;
+            }
+            var prev_dupeIndex = hash[curr_char].index;
+            var str_FromPrevDupe = input.substring(prev_dupeIndex + 1, i);
+            str = str_FromPrevDupe + curr_char;
+            hash = {};
+            for (var j = prev_dupeIndex + 1; j <= i; j++) {
+                hash[input.charAt(j)] = { index: j };
+            }
+        }
+    }
+    return longest_string.length > str.length ? longest_string : str;
 }
-console.log(Longest_Country_Name(["Australia", "Germany", "United States of America"]));
+console.log(longest_substring_without_repeating_characters("google.com"));
+
+console.log(longest_substring_without_repeating_characters("example.com")); 
